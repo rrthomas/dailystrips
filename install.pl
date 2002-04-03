0e82d3f155ad1a1392a3bdac7c08a606b8c75a3e
amedico <amedico>
2002-04-03 00:12:40 +0000

    More configurable

--- install.pl
+++ install.pl
@@ -7,8 +7,8 @@
 # Description:      installs dailystrips
 # Author:           Andrew Medico <amedico@amedico.dhs.org>
 # Created:          13 Jul 2001, 11:34 EST
-# Last Modified:    12 Jan 2001, 16:59 EST
-# Current Revision: 1.0.3
+# Last Modified:    02 Apr 2002, 18:59 EST
+# Current Revision: 1.0.4
 #
 
 
@@ -18,7 +18,7 @@ use strict;
 
 # Misc vars
 my (%options, $prog_version);
-$prog_version = "1.0.22";
+$prog_version = "1.0.23pre1";
 
 
 # Not for Win32
@@ -28,9 +28,9 @@ if ($^O =~ /Win32/ ) {
 
 
 # Editable paths
-$options{'installdir'} = "/usr/share/dailystrips";
-$options{'docsdir'} = "/usr/share/doc/dailystrips-$prog_version";
-$options{'bindir'} = "/usr/bin";
+$options{'sharedir'} = "/usr/share/dailystrips";
+$options{'docdir'} = "/usr/share/doc/dailystrips-$prog_version";
+$options{'scriptdir'} = "/usr/bin";
 
 
 # Help overrides anything else
@@ -41,9 +41,13 @@ Usage: $0 [OPTION]
 This program installs dailystrips. Options are as follows:
 
 Options:
-  -q  --quiet                turns off progress messages	
-      --verbose              turns on extra progress information, overrides -q
-      --installdir=DIR       installs to DIR instead of /usr/share/dailystrips/
+  -q  --quiet           turn off progress messages	
+      --verbose         turn on extra progress information, overrides -q
+      --sharedir=DIR    install shared files to DIR instead of
+                        /usr/share/dailystrips/
+      --scriptdir=DIR   install script to DIR instead of /usr/bin/
+      --docdir=DIR      install documentation to DIR/dailystrips-$prog_version
+                        instead of /usr/share/doc/dailystrips-$prog_version
 
 Bugs and comments to amedico\@amedico.dhs.org
 END_HELP
@@ -54,8 +58,12 @@ END_HELP
 
 # Parse options
 for (@ARGV)	{
-	if (/^(--installdir|-d)$/o) {
-		$options{'installdir'} = $1;
+	if (/^--sharedir=(.+)$/o) {
+		$options{'sharedir'} = $1;
+	} elsif (/^--scriptdir=(.+)$/o) {
+		$options{'scriptdir'} = $1;
+	} elsif (/^--docdir=(.+)$/o) {
+		$options{'docdir'} = "$1/dailystrips-$prog_version";
 	} elsif (/^(--quiet|-q)$/o) {
 		$options{'quiet'} = 1;
 	} elsif (/^--verbose$/o) {
@@ -72,33 +80,37 @@ if ($options{'verbose'} and $options{'quiet'}) {undef $options{'quiet'}}
 # Install:
 
 # defs:
-if ($options{'verbose'}) { warn "Installing definitions file to directory $options{'installdir'}\n" }
+if ($options{'verbose'}) { warn "Installing definitions file to directory $options{'sharedir'}\n" }
 
-if (system("install -d -o root $options{'installdir'}")) {
+if (system("install -d $options{'sharedir'}")) {
 	die "Error creating install directory. See above for reason.\n";
 }
 
-if (system("install -o root strips.def $options{'installdir'}/strips.def")) {
+if (system("install strips.def $options{'sharedir'}/strips.def")) {
 	die "Error installing definition file. See above for reason.\n";
 }
 
 
 # docs:
-if ($options{'verbose'}) { warn "Installing docs to directory $options{'docsdir'}\n" }
+if ($options{'verbose'}) { warn "Installing docs to directory $options{'docdir'}\n" }
 
-if (system("install -d -o root $options{'docsdir'}")) {
+if (system("install -d $options{'docdir'}")) {
 	die "Error creating documentation directory. See above for reason.\n";
 }
 
-if (system("install -o root BUGS CHANGELOG CONTRIBUTORS COPYING INSTALL README README.DEFS README.LOCAL TODO $options{'docsdir'}")) {
+if (system("install BUGS CHANGELOG CONTRIBUTORS COPYING INSTALL README README.DEFS README.LOCAL TODO $options{'docdir'}")) {
 	die "Error installing documentation files. See above for reason.\n";
 }
 
 
 # script:
-if ($options{'verbose'}) { warn "Installing scripts to directory $options{'bindir'}\n" }
+if ($options{'verbose'}) { warn "Installing scripts to directory $options{'scriptdir'}\n" }
 
-if (system("install -o root dailystrips dailystrips-clean $options{'bindir'}")) {
+if (system("install -d $options{'scriptdir'}")) {
+	die "Error creating scripts directory. See above for reason.\n";
+}
+
+if (system("install dailystrips dailystrips-clean $options{'scriptdir'}")) {
 	die "Error installing script. See above for reason.\n";
 }
 
