38a9e537ee903d39ea4bd06959ff11b5dd2337bd
amedico <amedico>
2003-08-25 04:40:51 +0000

    Updated to 1.0.28

--- dailystrips.pl
+++ dailystrips.pl
@@ -28,7 +28,7 @@ my (%options, $version, $time_today, @localtime_today, @localtime_yesterday, @lo
     $short_date_yesterday, $short_date_tomorrow, @get, @strips, %defs, $known_strips, %groups, $known_groups, %classes, $val,
     $link_tomorrow, $no_dateparse, @base_dirparts);
 
-$version = "1.0.28pre1";
+$version = "1.0.28
 
 $time_today = time;
 
